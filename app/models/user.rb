9be235c9b5afa37eb74063656eb39599f157f2b6
brandonking7 <kingrome74@gmail.com>
2017-06-03 14:55:53 -0400

    updated schema - also added class associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
